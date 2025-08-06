3acacdb4e5d358cb460a490c2f84c48b495c8a0f
Rakibul Islam <rakibulislam161363@gmail.com>
2025-08-06 07:42:50 +0600

    wheather  folder sob component vag kora ses

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Header from "./component/header/header"
+import Wheather from "./component/wheather/wheather"
 
 
 function App() {
  
 
   return (
-    <>
+    <div className="h-screen">
     <Header />
-    </>
+    <main className="mt-20">
+      <section>
+        <Wheather />
+      </section>
+    </main>
+    </div>
   )
 }
 
